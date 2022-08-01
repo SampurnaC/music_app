b845b9e766076012793f7ea3330048b0df253860
sam <sampurna.chapagainnn@gmail.com>
2022-08-01 12:49:43 +0100

    create react app using webpack- display songs with artists

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Artist from './components/Artist';
+import Song from './components/Song';
+
+const App = () => {
+  return (
+    <div>
+      <Artist></Artist>
+      <Song></Song>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Song.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import faint from '../images/faint.jpg';
+import newDivide from '../images/newDivide.jpg';
+import numb from '../images/numb.jpg';
+import whatIveDone from '../images/WhatIveDone.jpg';
+
+const Song = () => {
+  return (
+    <div className="song_container">
+      <div className="song_1">
+        <img src={faint} alt="Faint Song Cover Art" />
+        <h3>Faint</h3>
+      </div>
+      <div className="song_2">
+        <img src={newDivide} alt="New Divide Song Cover Art" />
+        <h3>New Divide</h3>
+      </div>
+      <div className="song_3">
+        <img src={numb} alt="Numb Song Cover Art" />
+        <h3>Numb</h3>
+      </div>
+      <div className="song_4">
+        <img src={whatIveDone} alt="What I've Done Song Cover Art" />
+        <h3>What I've Done</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Song;
